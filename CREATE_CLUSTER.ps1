82b1d8eceee4c5053b794ff9342a35ef1afd1627
nickwithey <67533343+nickwithey@users.noreply.github.com>
2024-09-22 17:40:31 +0100

    Add files via upload

---
+++ CREATE_CLUSTER.ps1
@@ -0,0 +1,31 @@
+﻿$json = get-content C:\sample.json -raw | convertfrom-json
+$nodes = $JSON.servers.Name
+foreach ($a in $nodes) {
+Install-WindowsFeature -Name "failover-clustering" -IncludeManagementTools
+write-host "Installing Failover Clustering on $($a)"
+}
+
+$clustername = $json.cluster.name
+$staticAddress = $json.cluster.StaticIP
+
+$clusterlist = [string]::Join(",",$Nodes)
+# $clusterlist = $clusterlist.Insert(0,''').Insert(22,''')
+
+
+# $clusterlist = $clusterlist.Insert(0,'''').Insert(20,'''')
+
+# New-Cluster -Name $clustername -Node "$($clusterlist)" -StaticAddress $staticAddress
+
+ # New-Cluster -Name $clustername -Node $clusterlist -StaticAddress $staticAddress
+
+
+$parameters = @{
+    Name = $clustername
+    Node = $clusterlist
+    StaticAddress = $staticaddress
+    }
+
+
+# New-Cluster @parameters
+
+new-cluster -name $clusterName -Node $clusterlist 
